f41344c93523088f97f43669d762de529389703c
Kalatshipi <99665715+Kalatshipi@users.noreply.github.com>
2023-02-11 11:47:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// change the nav style on scroll
+window.addEventListener("scroll", () => {
+    document.querySelector("header").classList.toggle("window-scrolled",window.scrollY > 0);
+})
+
+
+// ====== nav ======//
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
